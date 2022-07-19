668c6081e4f9936264646dda37ff90bc339e3b18
MBonjean <67752577+maous1@users.noreply.github.com>
2022-07-19 14:14:35 +0200

    version enrichment basic

--- Pandem2app.R
+++ Pandem2app.R
@@ -44,11 +44,11 @@ ui <- fluidPage(
         dataTableOutput("class")
       ),
       fluidRow(column(6,
-        numericInput("factor", "factor", 500,
-                     1, 10000000, 1)),
-        column(6,
-        selectInput("geolocalisation","Split by", choices = character())
-        )),
+                      numericInput("factor", "factor", 500,
+                                   1, 10000000, 1)),
+               column(6,
+                      selectInput("geolocalisation","Split by", choices = character())
+               )),
       fluidRow(
         actionButton("button", "Go simulate!"),
         dataTableOutput("knn"),
@@ -59,6 +59,27 @@ ui <- fluidPage(
       "4) Visualisation Knn",
       fluidRow(
         plotOutput("plotknn")
+
+      )
+
+    ),
+    tabPanel(
+      "5) enrichment",
+      fluidRow(
+        selectInput("variable","selectionner les variables", choices = "variant"),
+        selectInput("group","selectionner les groups", choices = character()),
+        selectInput("variant","selectionner le variant",choices = character()),
+        numericInput("multiplicateur", "multiplicateur", 1.2,
+                     1, 10000000, 0.1),
+        actionButton("button2", "Go enrichment"),
+        dataTableOutput("enrichment")
+      )
+
+    ),
+    tabPanel(
+      "6) Visualisation enrichment",
+      fluidRow(
+        plotOutput("plotenrichment")
 
       )
 
@@ -106,7 +127,7 @@ server <- function(input, output, session) {
   dataknn <- eventReactive(input$button, {
     simulator(trainset = dataVariant(),testset = dataAge(),geolocalisation =input$geolocalisation,outcome = input$var,count = 'new_cases', time= input$class,factor = input$factor)
 
-    })
+  })
 
   output$knn <- renderDataTable(dataknn(),options = list(pageLength = 5))
 
@@ -145,6 +166,28 @@ server <- function(input, output, session) {
     ggplot(data=dataknn(), aes(x=time, y=new_cases, fill=variant)) +
       geom_bar(stat="identity") + ggtitle("BE prediction") + theme(axis.text.x = element_text(angle = 90))+
       uniq_variant() + facet_wrap(vars(age_group))
+  }, res = 96)
+  observeEvent(dataknn(), {
+    updateSelectInput(session, "variable", choices = names(dataknn()))
+  })
+  observeEvent(dataknn(), {
+    updateSelectInput(session, "group", choices = unlist(unique(dataknn()[input$variable])))
+  })
+  observeEvent(dataknn(), {
+    updateSelectInput(session, "variant", choices = names(dataknn()))
+  })
+  enrichment <- eventReactive(input$button2,{
+    enrichment_variant(data_aggregated = dataknn(),
+                       variable = c("age_group"),group = c("<15yr"),
+                       variants = "B.1.1.7", multiplicateur = 1.2,time = "time")
+  })
+
+  output$enrichment <- renderDataTable(enrichment(),options = list(pageLength = 5))
+
+  output$plotenrichment <- renderPlot({
+    ggplot(data=enrichment(), aes(x=time, y=new_cases, fill=variant)) +
+      geom_bar(stat="identity") + ggtitle("with enrichment") +
+      theme(axis.text.x = element_text(angle = 90))+ xlab("Group") + ylab("number")+uniq_variant() + facet_wrap(vars(age_group))
   }, res = 96)
 
 }
